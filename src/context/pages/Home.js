874b2a29344d0b3cc2aa6d806849a09172bc2af4
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2023-01-07 16:46:51 +0300

    some modifications on the components

--- src/context/pages/Home.js
+++ src/context/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import Rooms from "../components/Rooms";
-import HeroSlider from "../components/HeroSlider";
-import Contact from "../components/Contact";
-import About from "../components/About";
+import Rooms from "../../components/Rooms";
+import HeroSlider from "../../components/HeroSlider";
+import Contact from "../../components/Contact";
+import About from "../../components/About";
 import { RoomContext } from "../RoomContext";
 
 const Home = () => {
--- src/context/pages/Signup.jsx
+++ src/context/pages/Signup.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import ScrollToTop from '../components/ScrollTop';
+import ScrollToTop from '../../components/ScrollTop';
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -26,17 +26,17 @@ const RegistrationForm = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     const res = await sendData();
-    if (res.st){
+    if (res.st) {
       alert('Registration Succesful! Please Login...')
       navigate(`/signin`)
       window.location.reload()
-    }else {
+    } else {
       alert(await res.mes)
       // window.location.reload()
     }
   };
 
-  const sendData = async() => {
+  const sendData = async () => {
     const res = await fetch(`http://localhost:8000/user/register/${formData.userType}/`, {
       method: "POST",
       headers: {
@@ -44,26 +44,26 @@ const RegistrationForm = () => {
       },
       body: JSON.stringify(formData)
     })
-    if (res.status !== 201){
+    if (res.status !== 201) {
       const data = await res.json()
       console.log(data)
-      return {'mes':data.message, 'st': false}
-    }else{
-      return {'mes':null, 'st':true}
+      return { 'mes': data.message, 'st': false }
+    } else {
+      return { 'mes': null, 'st': true }
     }
   }
 
   return (
     <>
-    <ScrollToTop/>
-    <div className="min-h-screen flex justify-center items-center bg-gray-50 py-28 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create your account
-          </h2>
-        </div>
-        {/* <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <ScrollToTop />
+      <div className="min-h-screen flex justify-center items-center bg-gray-50 py-28 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Create your account
+            </h2>
+          </div>
+          {/* <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
@@ -139,136 +139,136 @@ const RegistrationForm = () => {
             </button>
           </div>
         </form> */}
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-  <input type="hidden" name="remember" value="true" />
-  
-  {/* Name Field */}
-  <div>
-    <label htmlFor="name" className="sr-only">
-      Name
-    </label>
-    <input
-      id="name"
-      name="name"
-      type="text"
-      required
-      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-      placeholder="Name"
-      value={formData.name}
-      onChange={handleChange}
-    />
-  </div>
-  
-  {/* Email Field */}
-  <div>
-    <label htmlFor="email" className="sr-only">
-      Email address
-    </label>
-    <input
-      id="email"
-      name="email"
-      type="email"
-      autoComplete="email"
-      required
-      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-      placeholder="Email address"
-      value={formData.email}
-      onChange={handleChange}
-    />
-  </div>
-  
-  {/* Phone Field */}
-  <div>
-    <label htmlFor="phone" className="sr-only">
-      Phone
-    </label>
-    <input
-      id="phone"
-      name="phone"
-      type="tel"
-      autoComplete="tel"
-      required
-      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-      placeholder="Phone"
-      value={formData.phone}
-      onChange={handleChange}
-    />
-  </div>
-  
-  {/* Password Field */}
-  <div>
-    <label htmlFor="password" className="sr-only">
-      Password
-    </label>
-    <input
-      id="password"
-      name="password"
-      type="password"
-      autoComplete="new-password"
-      required
-      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-      placeholder="Password"
-      value={formData.password}
-      onChange={handleChange}
-    />
-  </div>
-  
-  {/* Confirm Password Field */}
-  <div>
-    <label htmlFor="confirm_password" className="sr-only">
-      Confirm Password
-    </label>
-    <input
-      id="password2"
-      name="password2"
-      type="password"
-      autoComplete="new-password"
-      required
-      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-      placeholder="Confirm Password"
-      value={formData.confirm_password}
-      onChange={handleChange}
-    />
-  </div>
-  
-      
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <input type="hidden" name="remember" value="true" />
 
-  {/* Submit Button */}
-  <div>
-    <button
-      type="submit"
-      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-        <svg
-          className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            d="M6 3a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2H6zm7 5a1 1 0 00-1-1H8a1 1 0 100 2h4a1 1 0 001-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </span>
-      <Link to="/signin">
-      Sign up
-      </Link>
-    </button>
-  </div>
-</form>
-<div className="text-center">
-      <p className="text-gray-600 text-sm">
-        Already have an account? 
-        <Link to="/signin" className="text-indigo-600 hover:underline"> Sign in here</Link>
-      </p>
-    </div>
+            {/* Name Field */}
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
 
+            {/* Email Field */}
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Phone Field */}
+            <div>
+              <label htmlFor="phone" className="sr-only">
+                Phone
+              </label>
+              <input
+                id="phone"
+                name="phone"
+                type="tel"
+                autoComplete="tel"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Phone"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Password Field */}
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Confirm Password Field */}
+            <div>
+              <label htmlFor="confirm_password" className="sr-only">
+                Confirm Password
+              </label>
+              <input
+                id="password2"
+                name="password2"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Confirm Password"
+                value={formData.confirm_password}
+                onChange={handleChange}
+              />
+            </div>
+
+
+
+            {/* Submit Button */}
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <svg
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6 3a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2H6zm7 5a1 1 0 00-1-1H8a1 1 0 100 2h4a1 1 0 001-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <Link to="/signin">
+                  Sign up
+                </Link>
+              </button>
+            </div>
+          </form>
+          <div className="text-center">
+            <p className="text-gray-600 text-sm">
+              Already have an account?
+              <Link to="/signin" className="text-indigo-600 hover:underline"> Sign in here</Link>
+            </p>
+          </div>
+
+        </div>
       </div>
-    </div>
     </>
   );
 };
